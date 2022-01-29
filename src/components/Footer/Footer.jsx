1ecababc282efb6b6a4bb57e6ff7fcea6ff1e94f
damkols <damolausmankolapo@gmail.com>
2022-01-29 14:04:17 -0800

    styled footer and fixed svg bugs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,42 +1,47 @@
 import React from "react";
 import "./Footer.scss";
-import facebook from "../../assets/icon-facebook.svg";
-import twitter from "../../assets/icon-twitter.svg";
-import instagram from "../../assets/icon-instagram.svg";
-import pinterest from "../../assets/icon-pinterest.svg";
+import Facebook from "../../assets/icon-facebook.svg";
+import Twitter from "../../assets/icon-twitter.svg";
+import Instagram from "../../assets/icon-instagram.svg";
+import Pinterest from "../../assets/icon-pinterest.svg";
 import Logo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="right">
-        <div className="logo">
-          <img src={Logo} alt="" />
+      <div className="container">
+        <div className="right">
+          <div className="logo">
+            <img src={Logo} alt="" />
+          </div>
+          <ul className="links">
+            <li>
+              <a href="">About</a>
+            </li>
+            <li>
+              <a href="">Careers</a>
+            </li>
+            <li>
+              <a href="">Events</a>
+            </li>
+            <li>
+              <a href="">Products</a>
+            </li>
+            <li>
+              <a href="">Support</a>
+            </li>
+          </ul>
         </div>
-        <ul className="links">
-          <li>
-            <a href="">About</a>
-          </li>
-          <li>
-            <a href="">Careers</a>
-          </li>
-          <li>
-            <a href="">Events</a>
-          </li>
-          <li>
-            <a href="">Products</a>
-          </li>
-        </ul>
-      </div>
-      <div className="left">
-        <div className="socials">
-          <img src={facebook} alt="" />
-          <img src={twitter} alt="" />
-          <img src={pinterest} alt="" />
-          <img src={instagram} alt="" />
-        </div>
-        <div className="copyrights">
-          <p> C 2021 Loopstudios, All rights reserved.</p>
+        <div className="left">
+          <div className="socials">
+            <img src={Facebook} alt="" />
+            <img src={Twitter} alt="" />
+            <img src={Pinterest} alt="" />
+            <img src={Instagram} alt="" />
+          </div>
+          <div className="copyrights">
+            <p> &copy; 2021 Loopstudios, All rights reserved.</p>
+          </div>
         </div>
       </div>
     </div>
